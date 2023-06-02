c1ce620779325bf199f89364b4490056773ebdb9
Rita Okonkwo <ritaokonkwo6@gmail.com>
2023-06-02 15:53:00 +0100

    set authed user id action

--- src/actions/authedUser.js
+++ src/actions/authedUser.js
@@ -1,7 +1,8 @@
 export const SET_AUTHED_USER = 'SET_AUTHED_USER'
 
 export const setAuthedUser = (user_id) => {
-    return (dispatch) => {
-        // TODO: after updating store with users
+    return {
+        type: SET_AUTHED_USER,
+        user_id
     }
 }
