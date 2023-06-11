a0056bcac2403428533320d76985ab0ce254b7fb
Rita Okonkwo <ritaokonkwo6@gmail.com>
2023-06-12 00:32:44 +0100

    add user navigation between answered and unanswered

--- src/components/dashboard/Dashboard.style.js
+++ src/components/dashboard/Dashboard.style.js
@@ -1,10 +1,23 @@
 import { makeStyles, shorthands} from '@fluentui/react-components';
 
 export const useStyles = makeStyles({
+  main: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
   title: {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
     ...shorthands.margin('1rem')
+  },
+  dashboard: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    ...shorthands.margin('1rem'),
+    width: '10rem'
   }
 });
